349494325ad8e59aebfb410ed84de71eacb24093
Ryan <zcabrdc@ucl.ac.uk>
2017-02-15 18:56:07 +0000

    changed posts to handle backend response

--- js/init.js
+++ js/init.js
@@ -5,10 +5,9 @@ $(document).ready(function(){
      name=$("#name").val();
      password=$("#password").val();
      $.post("http://localhost:3000/register",{name: name, email: email, password: password}, function(data){
-       if(data==='done')
-         {
-           console.log("sent");
-         }
+         if (data.allow) {
+             //window.location.href = "http://www.google.com";
+         } 
      });
    });
 
@@ -16,9 +15,10 @@ $(document).ready(function(){
      email=$("#emailL").val();
      password=$("#passwordL").val();
      $.post("http://localhost:3000/login",{email: email, password: password}, function(data){
-       if(data==='done')
-         {
-           console.log("sent");
+         if (data.allow) {
+             window.location.href = "http://www.google.com";
+         } else {
+             window.location.href = "http://www.bing.com";
          }
      });
    });

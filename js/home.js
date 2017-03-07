aa3dce689154cece6f6191580128a377f4e3d2b8
Ryan <zcabrdc@ucl.ac.uk>
2017-03-07 04:38:36 +0000

    Added more integration with backend

--- js/home.js
+++ js/home.js
@@ -1,79 +1,149 @@
 $(document).ready(function () {
+    var toastSuccess = $('<span>Success</span>');
+    var toastFailure = $('<span>Failure</span>');
+
+    //Form Options
     $('select').material_select();
 
     $('.datepicker').pickadate({
         selectMonths: true,
-        selectYears: 15 
+        selectYears: 15
     });
 
     $('.timepicker').pickatime({
         default: 'now',
-        twelvehour: false, 
+        twelvehour: false,
         donetext: 'OK',
         autoclose: false,
-        vibrate: true 
+        vibrate: true
     });
 
     $(".dropdown-button").dropdown();
 
     $('.modal').modal();
 
-    var token;
-    var contract, future, exdate, stime, duration, usd, wbs, reasonfree, otreason, nothours, rate, manager, revenue, paying;
-    var toastContent = $('<span>I am toast content</span>');
+    //Add a New User
+    $("#user_add").click(function () {
+        var user_das = $("#user_das").val();
+        var user_password = $("#user_password").val();
+        var user_confirm = $("#user_confirm").val();
+        var user_first_name = $("#user_first_name").val();
+        var user_last_name = $("#user_last_name").val();
+        var user_pay_roll = $("#user_pay_roll").val();
+        var user_location = $("#user_location").val();
+        var user_email = $("#user_email").val();
+        var user_alerts = $("#user_alerts").val();
+        var user_role = $("#user_role").val();
+        var user_access = $("#user_access").val();
+        var supervisor = document.cookie;
 
-    $("#resultsTable").append(
-        "<tr>" +
-        "<td>Alvin</td>" +
-        "<td>Eclair</td>"+
-        "<td><button type=\"button\" class=\"btn light-blue lighten-1 waves-effect waves-light\" id=\"submit\">Submit</button></td>" +
-        "</tr>"
-    );
-
-    $("#submit").click(function() {
-        token = document.cookie;
-
-        contract=$("#contract").val();
-        future=$("#future").val();
-        exdate=$("#exdate").val();
-        stime=$("#stime").val();
-        duration=$("#duration").val();
-        usd=$("#usd").val();
-        wbs=$("#wbs").val();
-        reasonfree=$("#reasonfree").val();
-        otreason=$("#otreason").val();
-        nothours=$("#nothours").val();
-        rate=$("#rate").val();
-        manager=$("#manager").val();
-        revenue=$("#revenue").val();
-        paying=$("#paying").val();
-        
-        $.post("http://localhost:3000/request", 
+        $.post("http://localhost:3000/adduser", 
         {
-            token: token,
-            contract: contract,
-            future: future,
-            exdate: exdate,
-            stime: stime,
-            duration: duration,
-            usd: usd,
-            wbs: wbs,
-            reasonfree: reasonfree,
-            otreason: otreason,
-            nothours: nothours,
-            rate: rate,
-            manager: manager,
-            revenue: revenue,
-            paying: paying
+            user_das: user_das,
+            user_password: user_password,
+            user_confirm: user_confirm,
+            user_first_name: user_first_name,
+            user_last_name: user_last_name,
+            user_pay_roll: user_pay_roll,
+            user_location: user_location,
+            user_email: user_email,
+            user_alerts: user_alerts,
+            user_role: user_role,
+            user_access: user_access,
+            supervisor: supervisor
         }, function(data){
             if (data.success) {
-                document.cookie = data.token;
-                Materialize.toast(toastContent, 5000);
+                Materialize.toast(toastSuccess, 5000);
+            } else {
+                Materialize.toast(toastFailure, 5000);
             }
         });
     });
 
+    //Request Submit
+    $("#request_submit").click(function() {
+        var das = document.cookie;
+        var request_contract=$("#request_contract").val();
+        var request_future=$("#request_future").val();
+        var request_date=$("#request_date").val();
+        var request_time=$("#request_time").val();
+        var request_duration=$("#request_duration").val();
+        var request_usd=$("#request_usd").val();
+        var request_wbs=$("#request_wbs").val();
+        var request_reason_free=$("#request_reason_free").val();
+        var request_reason_overtime=$("#request_reason_overtime").val();
+        var request_reason_hours=$("#request_reason_hours").val();
+        var request_rate=$("#request_rate").val();
+        var request_manager=$("#request_manager").val();
+        var request_revenue=$("#request_revenue").val();
+        var request_paying=$("#request_paying").val();
 
+        $.post("http://localhost:3000/request", 
+        {
+            das: das,
+            request_contract: request_contract,
+            request_future: request_future,
+            request_date: request_date,
+            request_time: request_time,
+            request_duration: request_duration,
+            request_usd: request_usd,
+            request_wbs: request_wbs,
+            request_reason_free: request_reason_free,
+            request_reason_overtime: request_reason_overtime,
+            request_reason_hours: request_reason_hours,
+            request_rate: request_rate,
+            request_manager: request_manager,
+            request_revenue: request_revenue,
+            request_paying: request_paying
+        }, function(data){
+            if (data.success) {
+                Materialize.toast(toastSuccess, 5000);
+            }
+        });
+
+        $.get("http://localhost:3000/review/"+document.cookie, function(data) {
+            for (var i = 0; i < data.results.length; i++) {
+                $("#review_results").append(
+                    "<tr>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                    "</tr>"
+                )
+            }
+        });
+    });
+
+    $.get("http://localhost:3000/review/"+document.cookie, function(data) {
+            for (var i = 0; i < data.results.length; i++) {
+                $("#review_results").append(
+                    "<tr>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                        "<td>Eclair</td>" +
+                    "</tr>"
+                )
+            }
+    });
     
 
+    // $("#resultsTable").append(
+    //     "<tr>" +
+    //         "<td>Alvin</td>" +
+    //         "<td>Eclair</td>"+
+    //         "<td><button type=\"button\" class=\"btn light-blue lighten-1 waves-effect waves-light\" id=\"submit\">Submit</button></td>" +
+    //     "</tr>"
+    // );
 });
--- js/init.js
+++ js/init.js
@@ -1,27 +1,16 @@
-$(document).ready(function(){
-   var email, name, password;
-   $("#register").click(function(){
-     email=$("#email").val();
-     name=$("#name").val();
-     password=$("#password").val();
-     $.post("http://localhost:3000/register",{name: name, email: email, password: password}, function(data){
-         if (data.allow) {
-             document.cookie = data.token;
-             window.location.href = "home.html";
-         }
-     });
-   });
+$(document).ready(function () {
+    var toastFailure = $('<span>Failure</span>');
 
-   $("#login").click(function(){
-     email=$("#emailL").val();
-     password=$("#passwordL").val();
-     $.post("http://localhost:3000/login",{email: email, password: password}, function(data){
-         if (data.allow) {
-             document.cookie = data.token;
-             window.location.href = "home.html";
-         } else {
-             alert("Login has failed"); //Change to something more appropriate later
-         }
-     });
-   });
- });
+    $("#login").click(function () {
+        var log_id = $("#log_id").val();
+        var log_password = $("#log_password").val();
+        $.post("http://localhost:3000/login", { log_id: log_id, log_password: log_password }, function (data) {
+            if (data.success) {
+                document.cookie = data.token;
+                window.location.href = "home.html";
+            } else {
+                Materialize.toast(toastFailure, 5000);
+            }
+        });
+    });
+});
